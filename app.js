ff2d0a5494f0d000fdd0021147670632aa0eac03
Raman Saran Verma <raman.verma@redbus.in>
2015-06-30 20:14:45 +0530

    npm compression

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var compression = require('compression');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var ytdwn = require('./routes/ytdwn');
 
 var app = express();
+app.use(compression());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
